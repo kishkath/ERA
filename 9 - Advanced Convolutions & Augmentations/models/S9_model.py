c73d230f8776ee7836f3a93e04bd085e5dea3800
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-06-28 08:08:47 +0530

    Update S9_model.py

--- 9 - Advanced Convolutions & Augmentations/models/S9_model.py
+++ 9 - Advanced Convolutions & Augmentations/models/S9_model.py
@@ -71,7 +71,7 @@ class NetArch(nn.Module):
         x = self.conv_block3(x)
         x = self.dropout(x)
         x = self.conv_block4(x)
-        x = x.reshape(-1,10)
+        x = x.reshape(-1,32)
         x = self.fc(x)
 
         return F.log_softmax(x,dim=-1)
