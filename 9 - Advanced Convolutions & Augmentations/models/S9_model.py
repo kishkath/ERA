0be5b2112ab779af0fb0ae810f6a9739aed85c1a
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-06-28 08:04:19 +0530

    Update S9_model.py

--- 9 - Advanced Convolutions & Augmentations/models/S9_model.py
+++ 9 - Advanced Convolutions & Augmentations/models/S9_model.py
@@ -44,24 +44,24 @@ class NetArch(nn.Module):
             nn.ReLU(),
             
             nn.Conv2d(128,128,3,padding=1,groups=128),
-            nn.Conv2d(128,64,1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(128,128,1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
 
 
-            nn.Conv2d(64,32,3,stride=2,padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(128,64,3,stride=2,padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU())
         
         self.conv_block4 = nn.Sequential(
-            nn.Conv2d(32,16,3,padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(64,32,3,padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(16,10,3,padding=1),
+            nn.Conv2d(32,32,3,padding=1),
             
             nn.AvgPool2d(4))
 
-        self.fc = nn.Linear(1*1*16,10)
+        self.fc = nn.Linear(1*1*32,10)
         self.dropout = nn.Dropout2d(0.05)
     def forward(self,x):
         x = self.conv_block1(x)
@@ -71,8 +71,8 @@ class NetArch(nn.Module):
         x = self.conv_block3(x)
         x = self.dropout(x)
         x = self.conv_block4(x)
-        x = x.view(-1,10)
-        #x = self.fc(x)
+        x = x.reshape(-1,10)
+        x = self.fc(x)
 
         return F.log_softmax(x,dim=-1)
             
