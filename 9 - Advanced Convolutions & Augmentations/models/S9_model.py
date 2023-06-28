1ac6bf9f7a41441546a36463ba419b3f8f000232
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-06-28 08:20:35 +0530

    Update S9_model.py

--- 9 - Advanced Convolutions & Augmentations/models/S9_model.py
+++ 9 - Advanced Convolutions & Augmentations/models/S9_model.py
@@ -17,10 +17,10 @@ class NetArch(nn.Module):
             nn.Conv2d(3,16,3,padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Conv2d(16,16,3,padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(16,32,3,padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(16,32,3,stride=2,padding=1),
+            nn.Conv2d(32,32,3,stride=2,padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU())
          
@@ -30,26 +30,25 @@ class NetArch(nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(),
            
-            nn.Conv2d(32,32,3,padding=1,dilation=2),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(32,64,3,padding=1,dilation=2),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
 
-            nn.Conv2d(32,64,3,stride=2,padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(64,128,3,stride=2,padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU())
         
         self.conv_block3 = nn.Sequential(
-            nn.Conv2d(64,128,3,padding=1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(128,128,3,padding=1,groups=128),
+            nn.Conv2d(128,64,1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             
-            nn.Conv2d(128,128,3,padding=1,groups=128),
-            nn.Conv2d(128,128,1),
-            nn.BatchNorm2d(128),
+            nn.Conv2d(64,64,3,padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-
-
-            nn.Conv2d(128,64,3,stride=2,padding=1),
+            
+            nn.Conv2d(64,64,3,stride=2,padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU())
         
@@ -57,9 +56,9 @@ class NetArch(nn.Module):
             nn.Conv2d(64,32,3,padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32,32,3,padding=1),
+            nn.Conv2d(32,32,3),
             
-            nn.AvgPool2d(4))
+            nn.AvgPool2d(2))
 
         self.fc = nn.Linear(1*1*32,10)
         self.dropout = nn.Dropout2d(0.05)
