fc11535c02de44cdd3f9d8b09dfd2a0ac59b0b04
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-06-30 18:09:00 +0530

    Update S9_model.py

--- 9 - Advanced Convolutions & Augmentations/models/S9_model.py
+++ 9 - Advanced Convolutions & Augmentations/models/S9_model.py
@@ -9,39 +9,50 @@ from torchsummary import summary
 ## Model
 # C1 C2 c3 P1 C3 C4 C5 c6 P2 C7 C8 C9 GAP C10
 import torch.nn as nn
-class NetArch(nn.Module):
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchsummary import summary
+
+
+## Model
+## Model
+# C1 C2 c3 P1 C3 C4 C5 c6 P2 C7 C8 C9 GAP C10
+import torch.nn as nn
+class NetArch3(nn.Module):
     def __init__(self):
-        super(NetArch,self).__init__()
+        super(NetArch3,self).__init__()
         
         self.conv_block1 = nn.Sequential(
-            nn.Conv2d(3,16,3,padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(3,20,3,padding=1),
+            nn.BatchNorm2d(20),
             nn.ReLU(),
-            nn.Conv2d(16,32,3,padding=1),
+            nn.Conv2d(20,32,3,padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.Conv2d(32,32,3,stride=2,padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(32,64,3,stride=2,padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU())
          
         
         self.conv_block2 = nn.Sequential(
-            nn.Conv2d(32,128,3,padding=1,dilation=2),
+            nn.Conv2d(64,128,3,padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
             
-            nn.Conv2d(128,64,3,padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(128,128,3,padding=1,groups=64),
+            nn.Conv2d(128,64,1),
             nn.ReLU(),
            
 
-            nn.Conv2d(64,64,3,stride=2,padding=1),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(64,32,3,stride=2,padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU())
         
         self.conv_block3 = nn.Sequential(
-            nn.Conv2d(64,64,3,padding=1,groups=64),
-            nn.Conv2d(64,64,1),
+            nn.Conv2d(32,64,3,padding=1,dilation=2),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
         
             nn.Conv2d(64,32,3,padding=1),
@@ -56,14 +67,12 @@ class NetArch(nn.Module):
             nn.Conv2d(32,16,3,padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
-            nn.Conv2d(16,10,4),
-            nn.BatchNorm2d(10),
-            nn.ReLU(),
+            nn.Conv2d(16,10,3,padding=1),
             
-            nn.AvgPool2d(1))
+            nn.AvgPool2d(3))
 
-        self.fc = nn.Linear(1*1*15,10)
-        self.dropout = nn.Dropout2d(0.025)
+        self.fc = nn.Linear(1*1*16,10)
+        self.dropout = nn.Dropout2d(0.045)
     def forward(self,x):
         x = self.conv_block1(x)
         x = self.dropout(x)
@@ -73,9 +82,14 @@ class NetArch(nn.Module):
         x = self.dropout(x)
         x = self.conv_block4(x)
         x = x.view(-1,10)
-        # x = self.fc(x)
+
 
         return F.log_softmax(x,dim=-1)
+            
+
+
+def return_summary(model,device, INPUT_SIZE):
+    return summary(model, input_size=INPUT_SIZE)
             
 
 
