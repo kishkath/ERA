fd4bc97fe9562f50e7699083843f99dcac1e7d2d
Kishkath <60026221+kishkath@users.noreply.github.com>
2023-07-03 22:25:47 +0530

    Update dataset.py

--- 10 - Resnet & OCP/utility/dataset.py
+++ 10 - Resnet & OCP/utility/dataset.py
@@ -67,7 +67,7 @@ class loader:
             A.PadIfNeeded (min_height=4, min_width=4,always_apply=False, p=0.5),
             A.RandomCrop (32,32, always_apply=False, p=0.5),
             #A.fliplr(),
-            A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=[0.4914*255, 0.4822*255, 0.4465*255], always_apply=False, p=0.5),
+            A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=[0.4914*255, 0.4822*255, 0.4465*255], always_apply=True, p=0.5),
             A.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.247, 0.243, 0.261]),
             A.pytorch.ToTensorV2()])
 
