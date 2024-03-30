a59434fd443bc25bb53dc0aef7c7af1e00ac1041
Kishkath <60026221+kishkath@users.noreply.github.com>
2024-03-30 12:41:26 +0530

    Update dataset.py
    removed additional paranthesis in line number 17

--- 16 - Transformer Speed Up/dataset.py
+++ 16 - Transformer Speed Up/dataset.py
@@ -14,7 +14,7 @@ class BilingualDataset(Dataset):
         ds = [item for item in ds if len(item['translation'][src_lang]) <= 150 and len(item['translation'][tgt_lang]) <= 160]
 
         # removing french sentences which has same or near by number of tokens to english language (len(french_tokens) - len(english_tokens) < =10)
-        self.ds = [item for item in ds if len(item['translation'][tgt_lang]) <= len(item['translation'][src_lang])) + 10]
+        self.ds = [item for item in ds if len(item['translation'][tgt_lang]) <= len(item['translation'][src_lang]) + 10]
         print("After preprocessing entire-dataset: ", len(self.ds))
         
         self.tokenizer_src = tokenizer_src
